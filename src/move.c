b40fe6db792c9a5536ec7709c36311281b63fbc4
Noah Huppert <contact@noahh.io>
2019-02-05 12:05:18 -0500

    numeric_move(): fixed implementation declaration to match header declaration

--- src/move.c
+++ src/move.c
@@ -1,6 +1,6 @@
 #include "move.h"
 
-void numeric_move(Selector from, Selector to, int diff, 
+void numeric_move(Selector *from, Selector *to, int diff, 
 		bool resize, bool same) {
 	// {{{1 Get all files with from prefix
 }
