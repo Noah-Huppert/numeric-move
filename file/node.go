5edbe7a923de2be68d63c21a8b58336e509e0245
Noah Huppert <contact@noahh.io>
2019-04-14 16:40:28 -0400

    moved file structs to file module

---
+++ file/node.go
@@ -0,0 +1,69 @@
+package file
+
+// NumPrefixFileNode is a node in a linked list which represents all numerically prefixed files in a directory
+// Representing these files as a linked list allows for easier shifting of prefixes.
+// Some nodes represent actual files. Some represent spaces in the prefix numbers.
+type NumPrefixFileNode struct {
+	// Type indicates the information the node stores
+	Type NumPrefixFileNodeType
+
+	// FileUnPrefixedName is the name of a file without its numeric prefix
+	FileUnPrefixedName string
+
+	// SpaceAmount is the amount of space between numeric prefixes
+	SpaceAmount uint64
+
+	// Next is a pointer to the next NumPrefixFileNode, nil if end of list
+	Next *NumPrefixFileNode
+
+	// Prev is the pointer to the previous NumPrefixFileNode, nil if beginning of list
+	Prev *NumPrefixFileNode
+}
+
+// NumPrefixFileNodeType is the type of a NumPrefixFileNode. Depending on this type value different fields in the
+// NumPrefixFileNode will be set.
+type NumPrefixFileNodeType uint32
+
+const (
+	// NodeTypeFile indicates that the node stores information about a file. Fields prefixed with File will be set.
+	NodeTypeFile NumPrefixFileNodeType = 0
+
+	// NodeTypeSpace indicates the the node stores information about a space between files. Fields prefixed with Space will be set.
+	NodeTypeSpace NumPrefixFileNodeType = 1
+)
+
+// InsertAfter adds a NumPrefixFileNode after the current
+func (n *NumPrefixFileNode) InsertAfter(i *NumPrefixFileNode) {
+	// If we are adding on to the end of the list
+	if n.Next == nil {
+		// from: x <-> n
+		// to  : x <-> n <-> i
+		n.Next = i
+		i.Prev = n
+	} else { // If inserting in the middle
+		// from: n <-> x
+		// to  : n <-> i <-> x
+		n.Next.Prev = i
+		i.Next = n.Next
+		i.Prev = n
+		n.Next = i
+	}
+}
+
+// InsertBefore adds a NumPrefixFileNode before the current
+func (n *NumPrefixFileNode) InsertBefore(i *NumPrefixFileNode) {
+	// If we are adding to the beginning of the list
+	if n.Prev == nil {
+		// from: n <-> x
+		// to  : i <-> n <-> x
+		i.Next = n
+		n.Prev = i
+	} else { // If inserting in the middle
+		// from: x <-> n
+		// to  : x <-> i <-> n
+		i.Prev = n.Prev
+		i.Prev.Next = i
+		n.Prev = i
+		i.Next = n
+	}
+}
--- main.go
+++ main.go
@@ -1,93 +1,14 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"path"
-	"regexp"
-	"path/filepath"
+	"os"
 	"strconv"
-	"sort"
+
+	"github.com/Noah-Huppert/numeric-move/file"
 
 	"github.com/Noah-Huppert/golog"
 )
-
-// numPrefixFileExp is a regular expression which matches against a numerically prefixed file
-var numPrefixFileExp *regexp.Regexp = regexp.MustCompile("^([0-9]+)(.*)$")
-
-// NumPrefixFile holds information about a numerically prefixed file
-type NumPrefixFile struct {
-	// Directory of file
-	Directory string
-
-	// UnPrefixedFileName is the file's name without the numerical prefix
-	UnPrefixedFileName string
-
-	// NumPrefix is the file's numerical prefix
-	NumPrefix uint64
-
-	// PrefixLength is the number of digits used to display a numerical prefix
-	PrefixLength uint64
-}	
-
-// NewNumPrefixFile creates a new NumPrefixFile from a path
-func NewNumPrefixFile(s string) (*NumPrefixFile, error) {
-	// Check if s is a path to a numerically prefixed file	
-	fName := path.Base(s)
-	matches := numPrefixFileExp.FindStringSubmatch(fName)
-	
-	if len(matches) == 0 {
-		return nil, fmt.Errorf("path is not a numerically prefixed file")
-	}
-
-	// Resolve to absolute path
-	absPath, err := filepath.Abs(s)
-	if err != nil {
-		return nil, fmt.Errorf("failed to resolve absolute path: %s", err.Error())
-	}
-
-	// Parse numerical prefix
-	num, err := strconv.ParseUint(matches[1], 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse numeric prefix: %s", err.Error())
-	}
-
-	return &NumPrefixFile{
-		Directory: path.Dir(absPath),
-		UnPrefixedFileName: matches[2],
-		NumPrefix: num,
-		PrefixLength: uint64(len(matches[1])),
-	}, nil
-}
-
-// Resize returns a new PrefixLength if the current one will not fit NumPrefix.
-// Otherwise 0 is returned.
-func (f NumPrefixFile) Resize() uint64 {
-	l := uint64(len(string(f.NumPrefix)))
-
-	if l > f.PrefixLength {
-		return l
-	}
-
-	return 0
-}
-
-// FileName returns the prefixed name of a file. Assumes PrefixLength can fit NumPrefix.
-func (f NumPrefixFile) FileName() string {
-	s := ""
-	numStr := string(f.NumPrefix)
-
-	for uint64(len(s) + len(numStr)) < f.PrefixLength {
-		s += "0"
-	}
-
-	return s + numStr
-}
-
-// Path is the file's full path. Assumes PrefixLength can fit NumPrefix.
-func (f NumPrefixFile) Path() string {
-	return filepath.Join(f.Directory, f.FileName())
-}
 
 func main() {
 	// {{{1 Setup logger
@@ -102,7 +23,7 @@ func main() {
 		fmt.Println("    NEW_PREFIX    New numerical prefix")
 	}
 
-	target, err := NewNumPrefixFile(os.Args[1])
+	target, err := file.NewNumPrefixFile(os.Args[1])
 	if err != nil {
 		logger.Fatalf("failed to parse target argument: %s", err.Error())
 	}
@@ -111,44 +32,35 @@ func main() {
 	if err != nil {
 		logger.Fatalf("failed to parse new prefix argument: %s", err.Error())
 	}
+
+	//newPrefixLength := len(string(newPrefix))
+
 	logger.Debugf("newPrefix: %d", newPrefix)
 
 	// {{{1 Model target directory files
-	files := []*NumPrefixFile{}
-
-	err = filepath.Walk(target.Directory, func(wPath string, info os.FileInfo, err error) error {
-		if info.IsDir() && wPath != target.Directory {
-			return filepath.SkipDir
-		}
-
-		fileName := path.Base(wPath)
-
-		if ! numPrefixFileExp.Match([]byte(fileName)) {
-			return nil
-		}
-
-		f, err := NewNumPrefixFile(wPath)
-		if err != nil {
-			return fmt.Errorf("failed to parse file into num prefix file: %s", err.Error())
-		}
-
-		files = append(files, f)
-
-		return nil
-	})
-
+	// {{{2 Create NumPrefixFile for each file
+	files, err := file.LoadDirectory(target.Directory)
 	if err != nil {
-		logger.Fatalf("failed to walk target directory: %s", err.Error())
+		logger.Fatalf("failed to model target directory: %s", err.Error())
 	}
 
-	sort.SliceStable(files, func(i, j int) bool {
-	    return files[i].NumPrefix < files[j].NumPrefix
-	})
-
-	for _, f := range files {
-		logger.Debugf("%#v", f)
+	// {{{2 Build linked list
+	/*filesHead := &file.NumPrefixFileNode{
+		Type:               file.NodeTypeFile,
+		FileUnPrefixedName: files[0].UnPrefixedName,
+		Next:               nil,
+		Prev:               nil,
 	}
+*/
 
-	// {{{1 Augment files for new target
-	// TODO: Something with a double loop
+	//currentNode := filesHead
+	lastPrefix := files[0].NumPrefix
+
+	for _, file := range files[1:] {
+		// Check if there is a space between this file and the last file
+		if file.NumPrefix-lastPrefix > 0 {
+			// Add spacer node
+
+		}
+	}
 }
